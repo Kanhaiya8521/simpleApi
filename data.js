2d99b1564cb5847f1a61236dbcf6cb6994bd9ca4
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2022-11-16 02:02:03 +0530

    this is simple API with out data bases

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Kanna', email:'kanna@gmail.com'},
+    {name:'Chaman', email:'chaman@gmail.com'},
+    {name:'Pratima', email:'pratima@gmail.com'}
+];
+module.exports = data;
